0ccd401c4e4991fac4f25fc4da43c899234d8b0b
Hatice <56474923+HaticeAkpolat@users.noreply.github.com>
2023-12-07 13:11:57 +0300

    OOP3 Interface

---
+++ OOP3/Program.cs
@@ -0,0 +1,37 @@
+﻿using OOP3;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Bu şekilde yazılması ile aşağıdaki gibi yazılması arasında bir fark yoktur!!!!!
+        //IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+        //ihtiyacKrediManager.Hesapla();
+
+        //TasitKrediManager tasitKrediManager = new TasitKrediManager();
+        //tasitKrediManager.Hesapla();
+
+        //KonutKrediManager konutKrediManager = new KonutKrediManager();
+        //konutKrediManager.Hesapla();
+
+        IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+        //ihtiyacKrediManager.Hesapla();
+
+        IKrediManager tasitKrediManager = new TasitKrediManager();
+        //tasitKrediManager.Hesapla();
+
+        IKrediManager konutKrediManager = new KonutKrediManager();
+        //konutKrediManager.Hesapla();
+        ILoggerService fileLoggerService = new FileLoggerService();
+        ILoggerService databaseLoggerService = new DatabaseLoggerService();
+
+        BasvuruManager basvuruManager = new BasvuruManager();
+        List<ILoggerService> loggerServices = new List<ILoggerService>() { databaseLoggerService,fileLoggerService};
+        basvuruManager.BasvuruYap(tasitKrediManager,loggerServices);
+
+        List<IKrediManager> krediler = new List<IKrediManager>() {tasitKrediManager ,konutKrediManager};
+        //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+    }
+}
